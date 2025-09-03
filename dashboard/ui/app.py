1823e69da67f08077bce013c9e3ca0fa9c3fcfa3
NikkiZed <niklas.lindkvist@homy.digital>
2025-09-03 22:54:02 +0200

    refactor(ui): removed unused code

--- dashboard/ui/app.py
+++ dashboard/ui/app.py
@@ -2,19 +2,11 @@ import time
 import dearpygui.dearpygui as dpg
 from ui.views import plain_dashboard, plot_view
 
-def save_callback():
-    print("Save Clicked")
-
 dpg.create_context()
 dpg.create_viewport(title="Example Window", width=1200, height=600, vsync=True)
 dpg.setup_dearpygui()
 
 with dpg.window(width=1200, height=600, no_collapse=True, no_resize=True):
-    dpg.add_text("Hello world")
-    dpg.add_button(label="Save", callback=save_callback)
-    dpg.add_input_text(label="string")
-    dpg.add_slider_float(label="float")
-
     with dpg.group(horizontal=True):
         with dpg.child_window(tag="panel:left", width=520, height=-1, border=True):
             with dpg.tab_bar(tag="tabs:left"):
