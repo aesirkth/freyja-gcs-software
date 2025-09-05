8a216751845681926afdc6726c4ae2ef50b42036
Hugo Frisk <hugopaf@outlook.com>
2025-09-05 14:35:12 +0200

    configure devicetree

--- app/src/main.c
+++ app/src/main.c
@@ -1,7 +1,32 @@
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
 
-void main(void) {
-    while(true) {
-        k_msleep(1000);
-    }
+#define LED0_NODE DT_NODELABEL(tgl0_led)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+int main(void)
+{
+	int ret;
+	bool led_state = true;
+
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return 0;
+		}
+
+		led_state = !led_state;
+		k_msleep(500);
+	}
+	return 0;
 }
