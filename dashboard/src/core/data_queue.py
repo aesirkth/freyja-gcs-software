4d0654a5ea4997ff561633845d07232d2ff7c88e
NikkiZed <niklas.lindkvist@homy.digital>
2025-09-06 16:37:19 +0200

    feature(ui) add telemetry data queue insert method

--- dashboard/src/core/data_queue.py
+++ dashboard/src/core/data_queue.py
@@ -1,6 +1,15 @@
+from models.input_data import TelemetryInput
+
 class DataQueue:
     def __init__(self):
-        pass
+        self.logger = logger = logging.getLogger(__name__)
+        self.tel_data_queue = []
+
+    def insert_data(data: TelemetryData):
+        try:
+            self.tel_data_queue.insert(0, data)
+        except Exception as e:
+            self.logger.e(f"Error while inserting telemetry data into queue {e}")
 
     def get_latest_data():
         pass
