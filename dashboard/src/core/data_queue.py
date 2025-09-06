547a3c1fd6c2fe5e221979c7a85a2e19eaf4e0e1
NikkiZed <niklas.lindkvist@homy.digital>
2025-09-06 16:39:07 +0200

    feature(ui) add latest telemetry data getter

--- dashboard/src/core/data_queue.py
+++ dashboard/src/core/data_queue.py
@@ -11,5 +11,14 @@ class DataQueue:
         except Exception as e:
             self.logger.e(f"Error while inserting telemetry data into queue {e}")
 
-    def get_latest_data():
-        pass
+    def get_latest_data() -> TelemetryInput:
+        try:
+            latest_data = self.tel_data_queue[0]
+
+            if isinstance(data, TelemetryInput):
+                return latest_data
+            else:
+                raise
+        except Exception as e:
+            self.logger.e(f"Error while getting latest telemetry data from queue {e}")
+
