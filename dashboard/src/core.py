a0809e2d01a7360f5a76ed15c38f016dfc81ff36
NikkiZed <niklas.lindkvist@homy.digital>
2025-09-08 19:16:30 +0200

    refactor(src) update error handling

--- dashboard/src/core.py
+++ dashboard/src/core.py
@@ -1,16 +1,21 @@
 import asyncio, random
 from dashboard.models.input_data import TelemetryInput
 from state.bus import telemetry_q
+import logging
+
+logger = logging.getLogger(__name__)
 
 async def core_loop():
-    while True:
-        tel = TelemetryInput(
-            speed=20 + random.random()*5,
-            battery_voltage=12.1,
-            tank_temp=35.0,
-        )
-        if telemetry_q.full():
-            _ = telemetry_q.get_nowait()
-        await telemetry_q.put(tel)
-        await asyncio.sleep(0.01) 
-        
+    try:
+        while True:
+            tel = TelemetryInput(
+                speed=20 + random.random()*5,
+                battery_voltage=12.1,
+                tank_temp=35.0,
+            )
+            if telemetry_q.full():
+                _ = telemetry_q.get_nowait()
+            await telemetry_q.put(tel)
+            await asyncio.sleep(0.01) 
+    except Exception as e:
+        logger.e(f"Error while running core loop. {e}")
