18714672dd61ed3a8caca78306fae3a788954424
NikkiZed <niklas.lindkvist@homy.digital>
2025-09-17 22:55:36 +0200

    cleanup: removed unnecessary  code and comments

--- dashboard/src/core_serial.py
+++ dashboard/src/core_serial.py
@@ -1,17 +1,14 @@
 import logging, asyncio
-import random
 from .core.can_conversion import read_and_apply_once
 from models.input_data import TelemetryInput
 from src.state.bus import telemetry_queue
-from serial.tools import list_ports
 import serial
 
 logger = logging.getLogger(__name__)
 
-async def core_serial_task(port="/dev/ttyUSB0", baud=115200):
+async def core_serial_task():
     ser = serial.Serial("/dev/cu.usbmodem112201", baudrate=9600, timeout=1)
     try:
-        # ports = list_ports.comports()
         latest_tel_data = TelemetryInput()
         while True:
             if read_and_apply_once(ser, latest_tel_data):
@@ -20,14 +17,6 @@ async def core_serial_task(port="/dev/ttyUSB0", baud=115200):
                 await telemetry_queue.put(latest_tel_data)
 
             await asyncio.sleep(0)
-            """
-            tel_data = TelemetryInput(
-                speed=random.random()*50,
-                battery_voltage=random.random()*50,
-                tank_temp=random.random()*50,
-            )
-            await telemetry_queue.put(tel_data)
-            """
     except Exception as e:
         logger.error(f"Error while running core serial task. {e}")
     finally:
