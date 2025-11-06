bbf1f32b812a89519856b65dc1140c7386bf1a3c
NikkiZed <niklas.lindkvist@homy.digital>
2025-11-06 14:59:28 +0100

    feat: add btn_launch and launch_armd read in main.c

--- app/src/main.c
+++ app/src/main.c
@@ -13,6 +13,9 @@ LOG_MODULE_REGISTER(main);
 const struct device *cdc_acm = DEVICE_DT_GET(DT_NODELABEL(cdc_acm_uart0));
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_NODELABEL(tgl0_led), gpios);
+static const struct gpio_dt_spec btn_launch = GPIO_DT_SPEC_GET(DT_NODELABEL(btn_launch), gpios);
+static const struct gpio_dt_spec launch_armd = GPIO_DT_SPEC_GET(DT_NODELABEL(launch_armd), gpios);
+
 
 int main(void) {
 	int ret;
@@ -33,21 +36,33 @@ int main(void) {
 		return 0;
 	}
 
-	if (!gpio_is_ready_dt(&led)) {
+	if (!gpio_is_ready_dt(&led) || !gpio_is_ready_dt(&btn_launch) || !gpio_is_ready_dt(&launch_armd)) {
 		return 0;
 	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
-		LOG_ERR("could not configure pin");
+		LOG_ERR("Failed to configure pin");
 		return 0;
 	}
+
+    ret = gpio_pin_configure_dt(&btn_launch, GPIO_INPUT);
+    if (ret) {
+        LOG_ERR("Failed to configure btn_launch", ret);
+        return;
+    }
+
+    ret = gpio_pin_configure_dt(&launch_armd, GPIO_INPUT);
+    if (ret) {
+        LOG_ERR("Failed to configure launch_armd", ret);
+        return;
+    }
+
 	ret = gpio_pin_set_dt(&led, 1);
 	if (ret < 0) {
 		LOG_ERR("could not set pin");
 		return 0;
 	}
-
 
 	LOG_INF("started");
 
@@ -56,6 +71,15 @@ int main(void) {
 		get_timestamp(&timestamp);
 		submit_usb_pkt(&pkt, PKT_TYPE_THRUST, timestamp);
 		submit_can_pkt(&pkt, PKT_TYPE_THRUST);
+
+		int val_launch = gpio_pin_get_dt(&btn_launch);
+        int val_armd = gpio_pin_get_dt(&launch_armd);
+
+        if (val_launch < 0 || val_armd < 0) {
+            LOG_ERR("Failed to read pins");
+        } else {
+            LOG_INF("btn_launch = %d, launch_armd = %d", val_launch, val_armd);
+        }
 		k_msleep(1000);
 	}
 	return 0;
