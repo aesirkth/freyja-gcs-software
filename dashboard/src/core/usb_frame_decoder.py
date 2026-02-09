d34c96a7e926b685ca9bc4fc76cce4bfb4007099
NikkiZed <niklas.lindkvist@homy.digital>
2026-02-09 11:05:37 +0100

    cleanup

--- dashboard/src/core/usb_frame_decoder.py
+++ dashboard/src/core/usb_frame_decoder.py
@@ -1,7 +1,4 @@
 import serial, logging
-from .timestamp_decoder import apply_unix_timestamp
-from models.input_tm_data import TelemetryInput
-from models.gcs_state import GCSState
 from config.decoder_config import SYNC0, SYNC1
 import zlib
 
@@ -99,28 +96,3 @@ class UsbFrameDecoder:
         except Exception as e:
             logger.error(f"Error while reading GSE USB frame. {e}")
             return None
-
-def read_next_frame_and_apply(ser: serial.Serial, empty_tel_object: TelemetryInput, empty_gcs_state_object: GCSState) -> bool:
-    try:
-        frame = read_usb_frame(ser)
-        if not frame:
-            return False
-      
-        usb_id, usb_pkt_timestamp, usb_pkt_payload = frame
-        if not usb_id or not usb_pkt_payload:
-            return False
-
-        decode_pkt = DECODERS.get(usb_id)
-        if decode_pkt:
-            if usb_id == 0x700:
-                decode_pkt(usb_pkt_payload, empty_gcs_state_object)
-                apply_unix_timestamp(usb_pkt_timestamp, empty_gcs_state_object)
-            else:
-                decode_pkt(usb_pkt_payload, empty_tel_object)
-                apply_unix_timestamp(usb_pkt_timestamp, empty_tel_object)
-            return True
-       
-        return False
-    except Exception as e:
-        logger.error(f"Error while reading and applying bytes to target object. {e}")
-        return None
