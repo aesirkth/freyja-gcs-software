f615cd13099185b133764d9b0c2332537456f5de
NikkiZed <niklas.lindkvist@homy.digital>
2026-02-20 16:51:27 +0100

    cleanup

--- dashboard/backend/main.py
+++ dashboard/backend/main.py
@@ -2,6 +2,7 @@ import traceback
 import json
 from fastapi import FastAPI, WebSocket
 from src.state.cmd_queue import cmd_queue
+from src.utils.format_msg import format_message
 from pathlib import Path
 import asyncio
 import subprocess
@@ -10,8 +11,10 @@ from src.state.con_manager import ConnectionManager
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from src.core_serial import core_serial_task
+import logging
 
 app = FastAPI()
+logger = logging.getLogger(__name__)
 # app.include_router(api_router)
 
 socket_manager = ConnectionManager()
@@ -24,7 +27,7 @@ async def startup_event():
         # subprocess.run(["xdotool", "key", "F5"], env={"DISPLAY": ":0"})
         print("Kiosk refresh signal sent!")
     except Exception as e:
-        print(f"Failed to refresh kiosk: {e}")
+        logger.error(f"Failed to refresh kiosk: {e}")
         traceback.print_exc()
         raise e
 
@@ -33,14 +36,12 @@ async def websocket_endpoint(websocket: WebSocket):
     await socket_manager.connect(websocket)
     while True:
         data = await websocket.receive_text()
-        # print(f"Received data: {data}")
+        print(f"Received data: {data}")
         
         await cmd_queue.put(data)
-        payload = {
-            "data": data,
-        }
+        payload = format_message(data)
         await websocket.send_text(json.dumps(payload))
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(0)
 
 FRONTEND_DIST = Path(__file__).resolve().parent.parent / "frontend" / "dist"
 app.mount("/", StaticFiles(directory=str(FRONTEND_DIST), html=True), name="static")
